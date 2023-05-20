332e6ac20fede77bbcebc5a50a7d41fd7eb72acc
coman99us <intotheinfinite8universe@gmail.com>
2023-05-20 19:15:52 +0300

    Revert "S"
    This reverts commit 5a4f0c4a6e71d783bd791faf04aac40a01a81c5d.

--- task07/task07.py
+++ task07/task07.py
@@ -10,26 +10,6 @@ def get_max_salary(f, max_sal_local):
     return max_sal_local
 
 
-def get_sum(f, month):
-    partial_sum=0
-    for line in f.readlines()[1:]:
-        if month in line or month.lower() in line:
-            data = line.split(",")
-            data[1] = int(data[1])
-            partial_sum += data[1]
-    return partial_sum
-
-def get_avg(f, months):
-    partial_sum=0
-    no_of_entries = 0
-    for line in f.readlines()[1:]:
-        if months in line or months.lower() in line:
-            data = line.split(",")
-            data[1] = int(data[1])
-            partial_sum += data[1]
-    return partial_sum, no_of_entries
-
-
 wb = Workbook()
 
 sheet1 = wb.add_sheet("Sheet 1")
@@ -38,6 +18,11 @@ f1 = open("f1.txt", "r")
 f2 = open("f2.txt", "r")
 f3 = open("f3.txt", "r")
 
+'''
+print(f1.readline())
+print(f2.readline())
+print(f3.readline())
+'''
 
 max_sal_local = 0
 
@@ -50,31 +35,28 @@ print(max_sal)
 
 sheet1.write(0, 1, "Salariu maxim")
 sheet1.write(1, 1, str(max_sal))
-sheet1.write(0, 2, "suma_salariilor - Octombrie")
-sheet1.write(0, 3, "media_salariilor")
-sheet1.write(1, 3, "total_sum")
-
-
 
 wb.save("example.xls")
 
-f1.seek(0)
-f2.seek(0)
-f2.seek(0)
+'''
+for lst in [f1.readlines(), f2.readlines(), f3.readlines()]:
+    for line in lst:
+        data = line.split(",")
+        data[1] = int(data[1])
+        if max_sal < data[1]:
+            max_sal = data[1]
+'''
 
-month = 'Octombrie'
-# sum_f1, no_of_entriesf1 = get_sum(f1, month)
-t1 = get_sum(f1, month)
-sum_f2 = get_sum(f2, month)
-sum_f3 = get_sum(f3, month)
-total_sum = sum([sum_f1, sum_f2, sum_f3])
-print(total_sum)
+'''
+for line in f2.readlines():
+    data = line.split(",")
+    data[1] = int(data[1])
+    if max_sal < data[1]:
+        max_sal = data[1]
 
-month = 'Octombrie', 'Noiembrie'
-sum_f1 = get_avg(f1, months)
-sum_f2 = get_avg(f2, months)
-sum_f3 = get_avg(f3, months)
-total_avg= sum([sum_f1, sum_f2, sum_f3])
-
-
-close()
+for line in f3.readlines():
+    data = line.split(",")
+    data[1] = int(data[1])
+    if max_sal < data[1]:
+        max_sal = data[1]
+'''
