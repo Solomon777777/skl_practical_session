aabda32e1510fe31a34b2910526c8b046639ea58
coman99us <intotheinfinite8universe@gmail.com>
2023-05-22 19:36:09 +0300

    no message

--- task03/task03.py
+++ task03/task03.py
@@ -1,12 +1,15 @@
-import io
+from typing import Any
 
+'''
 def func(var, value, a = 5, b = 7, c = 3, d = 2):
-	res = a * (value ** 3) - b * (value ** 2) - c * value + d
-	if a > 3 and b < 2:
-		var.write(str(a / b))
+	res = int(a * (value ** 3) - b * (value ** 2) - c * value + d)
+	if a > 3 or b < 2:
+		pass
+	else:
+		var.write(int(a / b))
 	if c > 2 and d > 0:
 		try:
-			var.write(str(value / (d // c)))
+			var.write(int(value / (d // c)))
 		except Exception as e:
 			var.write("A fost intalnita o exceptie" + "\n")
 	if a ** d == 25:
@@ -23,3 +26,35 @@ if __name__ == '__main__':
 	res = func(var, value)
 	var.write(str(value) + "\n")
 	var.close()
+	
+
+	'''
+def func(var, value, a=5, b=7, c=3, d=2):
+    res = a * (value ** 3) - b * (value ** 2) - c * value + d
+    if a > 3 and b < 2:
+        var.write(str(a / b))
+    if c > 2 and d > 0:
+        try:
+            var.write(str(value / (d // c)))
+        except Exception as e:
+            var.write("A fost intalnita o exceptie" + "\n")
+    if a ** d == 25:
+        var.write(str(a ** c))
+    var.write(str(a * b + c * d) + "\n")
+    return res
+
+
+if __name__ == '__main__':
+    print("Please enter a filename:")
+    name = str(input())
+    print("Please enter a integer value:")
+    value = int(input())
+    try:
+        with open(name, "w+") as var:
+            res = func(var, value)
+            var.write(str(value) + "\n")
+    except Exception as e:
+        print("A fost intalnita o exceptie: ", str(e))
+
+
+
